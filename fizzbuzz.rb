70dffc6a4594a1bb9bd42932856be3a0f3687ede
reiya8gC <xqmp71180@mineo.jp>
2023-11-30 15:09:11 +0900

    ADD fizzbuzz.rb ver.2

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 for i in 0..100 do
-    if i % 3 == 0
-        p"#{i}, fizz"
+    if i % 15 == 0
+        p"#{i}, fizzbuzz"
     elsif i % 5 == 0 
         p"#{i}, buzz"
-    elsif i % 15 == 0
-        p"#{i}, fizzbuzz"
+    elsif i % 3 == 0
+        p"#{i}, fizz"
     else
         p"#{i}"
     end
